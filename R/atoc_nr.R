7ea526861a2993904817261575fd69b14a28c8c3
Daniel Chick <dan@zipabout.com>
2024-09-10 21:43:30 +0100

    Reversion to := operator, with library import

--- R/atoc_nr.R
+++ R/atoc_nr.R
@@ -1,3 +1,5 @@
+library(data.table)
+
 #' ATOC to GTFS (Network Rail Version)
 #'
 #' Convert ATOC CIF files from Network Rail to GTFS
@@ -160,12 +162,15 @@ nr2gtfs <- function(full_path,
 }
 
 process_updates_incremental <- function(schedule_df, stop_times_df, update_paths, silent = TRUE, full_import = FALSE) {
+  # Import data.table namespace
+  import("data.table")
+
   # Convert to data.table for efficient processing
   schedule_df <- data.table::as.data.table(schedule_df)
   stop_times_df <- data.table::as.data.table(stop_times_df)
 
   # Add schedule_id to the main dataframe once
-  schedule_df <- schedule_df[, c(.SD, list(schedule_id = paste(`Train UID`, `Date Runs From`, `STP indicator`, sep = "_")))]
+  schedule_df[, schedule_id := paste(`Train UID`, `Date Runs From`, `STP indicator`, sep = "_")]
 
   for (update_path in update_paths) {
     if (!silent) {
@@ -187,8 +192,7 @@ process_updates_incremental <- function(schedule_df, stop_times_df, update_paths
     gc()
 
     # Add schedule_id to update data
-    update_schedule <- update_schedule[, c(.SD, list(schedule_id = paste(`Train UID`, `Date Runs From`, `STP indicator`, sep = "_")))]
-
+    update_schedule[, schedule_id := paste(`Train UID`, `Date Runs From`, `STP indicator`, sep = "_")]
 
     # Process each schedule in the update file
     for (i in 1:nrow(update_schedule)) {
@@ -236,7 +240,7 @@ process_updates_incremental <- function(schedule_df, stop_times_df, update_paths
   }
 
   # Remove temporary schedule_id column
-  schedule_df <- schedule_df[, !("schedule_id"), with = FALSE]
+  schedule_df[, schedule_id := NULL]
 
   # Convert back to data.frame
   schedule_df <- as.data.frame(schedule_df)
